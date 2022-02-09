1a16b5ddcd94b03a8175a95adb478a775fd5c6f6
Nasrullah Khan <nk107777@gmail.com>
2022-02-09 17:55:55 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
@@ -9,9 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Header />
       <div className='container'>
-        <Switch>
-          <Route path='/' component={Home} />
-        </Switch>
+        <Route path='/' component={Home} />
       </div>
     </BrowserRouter>
   );
